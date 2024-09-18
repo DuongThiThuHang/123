4fefd074d4a2e8f6c9f282f19da933562051dffd
DuongThiThuHang <23010668@st.phenikaa-uni.edu.vn>
2024-09-18 23:10:50 +0700

    Add files via upload

---
+++ dog3.py
@@ -0,0 +1,5 @@
+from animal3 import Animal3
+
+class Dog(Animal3):
+    def bark(self):
+        return "barking..."
---
+++ randomlist.py
@@ -0,0 +1,7 @@
+import random
+class RandomList:
+    def __init__(self, item):
+        self.item = item
+
+    def get_random_element(self):
+        return self.item.removes()
